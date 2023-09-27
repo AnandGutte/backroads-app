601e578210165d2c5e99a914c666a4f2381d032d
Anand PC <anand.gutte24@gmail.com>
2023-09-27 06:44:43 +0530

    first commit

---
+++ src/components/SocialLinks.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import { socialLinks } from '../data';
+const PageIcons = ({ parentClass, itemClass }) => {
+  return (
+    <ul className={parentClass}>
+      {socialLinks.map((link) => {
+        const { id, href, icon } = link;
+        return (
+          <li key={id}>
+            <a
+              href={href}
+              target="_blank"
+              rel="noreferrer"
+              className={itemClass}
+            >
+              <i className={icon}></i>
+            </a>
+          </li>
+        );
+      })}
+    </ul>
+  );
+};
+
+export default PageIcons;
